4b3ffd2c85241f2a0dd163640c7b322ff03de771
kimtechnos <franciskimani559@gmail.com>
2024-05-11 15:24:00 +0300

    second commit

--- dlmsdb.sql
+++ dlmsdb.sql
@@ -34,7 +34,7 @@ CREATE TABLE `tbladmin` (
   `UserName` varchar(50) DEFAULT NULL,
   `MobileNumber` bigint(10) DEFAULT NULL,
   `Email` varchar(120) DEFAULT NULL,
-  `Password` varchar(200) DEFAULT NULL,
+  `Password` varchar(300) DEFAULT NULL,
   `AdminRegdate` timestamp NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
